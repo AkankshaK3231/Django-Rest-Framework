b882ed484c163f3159f2884a2d3258be2ec2787e
Akanksha3231 <akankshakalbhor3231@gmail.com>
2025-10-08 19:30:17 +0530

    Django Rest Framework Practice until serializers

---
+++ home/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Person
+
+class PeopleSerializer(serializers.ModelSerializer):
+    class Meta :
+        model = Person
+        fields = '__all__'
+
+    def validate(self,data):
+
+        special_characters = "!@#$%^&*()_+-=<>,/"
+        if any(c in special_characters for c in data['name']):
+            raise serializers.ValidationError('name cannot contain special chars')
+
+        if data['age']<18:
+            raise serializers.ValidationError('age should be greater than 18')
+        return data
---
+++ home/views.py
@@ -0,0 +1,64 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from home.models import Person
+from home.serializers import PeopleSerializer
+
+# Create your views here.
+@api_view(['GET','POST','PUT'])
+def index(request):
+        courses = {
+            'course_name' : 'Python',
+            'learn' : ['flask', 'Django','Tornado','FastApi']
+        }
+
+        if request.method == 'GET':
+            print('You hit a GET method')
+            return Response(courses)
+        elif request.method == 'POST':
+            data = request.data 
+            print(data)
+            print('You hit a POST method')
+            return Response(courses)
+
+        elif request.method == 'PUT':
+            print('You hit a PUT method')
+            return Response(courses)
+
+@api_view(['GET','POST','PUT', 'PATCH','DELETE'])
+def person(request):
+    if request.method == 'GET' :
+        objs = Person.objects.all()
+        serializer = PeopleSerializer(objs, many = True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        data = request.data
+        serializer = PeopleSerializer(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    
+    elif request.method == 'PUT':
+        data = request.data
+        serializer = PeopleSerializer(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    
+    elif request.method == 'PATCH':
+        data = request.data
+        obj = Person.objects.get(id = data['id'] )
+        serializer = PeopleSerializer(obj, data = data, partial = True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    
+    else :
+        data = request.data
+        obj = Person.objects.get(id = data['id'] )
+        obj.delete()
+        return Response({'message' : 'person deleted'})
