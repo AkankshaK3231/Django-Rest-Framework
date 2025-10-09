40fa581f5df0fa79e0b7bbd0dcb9eb3dc3d9c152
AkankshaK3231 <akanksha.kalbhor@vervelo.com>
2025-10-09 17:57:14 +0530

    Status, Token, Permissions Authentication, user register and login

--- home/serializers.py
+++ home/serializers.py
@@ -1,18 +1,54 @@
 from rest_framework import serializers
 from .models import Person, Color
+from django.contrib.auth.models import User
+
+class RegisterSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    email = serializers.EmailField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+
+        if data['username']:
+            if User.objects.filter(username = data['username']).exists():
+                raise serializers.ValidationError('username is taken')
+
+        if data['email']:
+            if User.objects.filter(email = data['email']).exists():
+                raise serializers.ValidationError('email is taken')
+
+        return data
+    
+    def create(self,validated_data):
+        user = User.objects.create(username = validated_data['username'], email = validated_data['email'])
+        user.set_password(validated_data['password'])
+        user.save()
+        return validated_data
+        print(validated_data)
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
 
 class ColorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Color
-        fields = ['color_name']
+        fields = ['color_name','id']
 
 
 class PeopleSerializer(serializers.ModelSerializer):
-    color = ColorSerializer()
+    #color = ColorSerializer()
+    #color_info = serializers.SerializerMethodField()
+
     class Meta :
         model = Person
         fields = '__all__'
         
+    def get_color_info(self,obj):
+        color_obj = Color.objects.get(id = obj.color.id)
+        return {'color_name' : color_obj.color_name, 'hex_code': "#000" }
 
     def validate(self,data):
 
--- home/views.py
+++ home/views.py
@@ -2,34 +2,143 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from home.models import Person
-from home.serializers import PeopleSerializer
+from home.serializers import PeopleSerializer , LoginSerializer , RegisterSerializer
 
+from rest_framework.views import APIView
+from rest_framework import viewsets
+from rest_framework import status
+
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+
+class LoginAPI(APIView):
+    def post(self,request):
+        data = request.data
+        serializer = LoginSerializer(data  = data)
+        if not serializer.is_valid():
+            return Response({
+                'status' : False,
+                'message' : serializer.errors
+            }, status.HTTP_400_BAD_REQUEST)
+
+        print(serializer.data)
+        user = authenticate(username = serializer.data['username'],password = serializer.data['password'])
+        if not user :
+            return Response({
+                'status' : False,
+                'message' : 'Invalid Credentials'
+            }, status.HTTP_400_BAD_REQUEST)
+
+        token, _ = Token.objects.get_or_create(user=user)
+        print(token)
+        return Response({'status': True , 'message': 'user login', 'token' : str(token)}, status = status.HTTP_201_CREATED )
+
+class RegisterAPI(APIView):
+
+    def post(self,request):
+        data = request.data
+        serializer = RegisterSerializer(data = data)
+
+        if not serializer.is_valid():
+            return Response({
+                'status' : False,
+                'message' : serializer.errors
+            }, status.HTTP_400_BAD_REQUEST)
+
+        serializer.save()
+
+        return Response({'status': True , 'message': 'user created'}, status = status.HTTP_201_CREATED )
 # Create your views here.
-@api_view(['GET','POST','PUT'])
+@api_view(['GET','POST'])
 def index(request):
-        courses = {
-            'course_name' : 'Python',
-            'learn' : ['flask', 'Django','Tornado','FastApi']
-        }
 
         if request.method == 'GET':
-            print('You hit a GET method')
-            return Response(courses)
-        elif request.method == 'POST':
+            json_response = {
+                'name':'Akanksha',
+                'courses' : ['c++','python'],
+                'method' : 'GET'
+            }
+        else:
             data = request.data 
             print(data)
-            print('You hit a POST method')
-            return Response(courses)
+            json_response = {
+                'name':'Akanksha',
+                'courses' : ['c++','python'],
+                'method' : 'POST'
+            }
+        return Response(json_response)
+            
 
-        elif request.method == 'PUT':
-            print('You hit a PUT method')
-            return Response(courses)
+@api_view(['POST'])
+def login(request):
+    data = request.data
+    serializer = LoginSerializer(data = data)
+
+    if serializer.is_valid():
+        data = serializer.data
+        print(data)
+        return Response({'message' : 'success'})
+
+    return Response(serializer.errors)
+
+
+class PersonAPI(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
+    def get(self,request):
+        print(request.user)
+        objs = Person.objects.filter(color_isnull = False)
+        serializer = PeopleSerializer(objs, many = True)
+        return Response(serializer.data)
+        
+    
+    def post(self,request):
+        data = request.data
+        serializer = PeopleSerializer(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+      
+    
+    def put(self,request):
+        data = request.data
+        serializer = PeopleSerializer(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+        
+    
+    def patch(self,request):
+        data = request.data
+        obj = Person.objects.get(id = data['id'] )
+        serializer = PeopleSerializer(obj, data = data, partial = True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    
+    def delete(self,request):
+        data = request.data
+        obj = Person.objects.get(id = data['id'] )
+        obj.delete()
+        return Response({'message' : 'person deleted'})
+
 
 @api_view(['GET','POST','PUT', 'PATCH','DELETE'])
 def person(request):
+
     if request.method == 'GET' :
         objs = Person.objects.filter(color_isnull = False)
         serializer = PeopleSerializer(objs, many = True)
+        serializer_context = {
+            'request':(request),
+        }
+        context = serializer_context
         return Response(serializer.data)
     
     elif request.method == 'POST':
@@ -62,3 +171,18 @@ def person(request):
         obj = Person.objects.get(id = data['id'] )
         obj.delete()
         return Response({'message' : 'person deleted'})
+    
+
+
+class PeopleViewSet(viewsets.ModelViewSet):
+    serializer_class = PeopleSerializer
+    queryset = Person.objects.all()
+
+    def list(self,request):
+        search = request.GET.get('search')
+        queryset = self.queryset
+        if search:
+            queryset.filter(name_startswith = search)
+
+        serializer = PeopleSerializer(queryset, many = True)
+        return Response({'status' : 200 , 'data' : serializer.data}, status = status.HTTP_204_NO_CONTENT) 
